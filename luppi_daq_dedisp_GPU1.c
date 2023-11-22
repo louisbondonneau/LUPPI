5e2a02e966b4a5e8c299137480cbe5539b5342bb
bondonneau louis <louis.bondonneau@me.com>
2023-11-22 11:41:45 +0100

    do not move old parset and old log anymore

--- luppi_daq_dedisp_GPU1.c
+++ luppi_daq_dedisp_GPU1.c
@@ -170,8 +170,7 @@ int main(int argc, char *argv[]) {
     // -- preparing upload scripts -
 
     char string[1024], filename[128], hostname[128], strlog[128], logname[128], logdir[128];
-    char source[256], source_dir[256], mode[256] , path[256], commande[2048], date[128];
-
+    char source[256], mode[256] , path[256], commande[2048], date[128];
 
     hgets(stat.buf, "SRC_NAME", 256, source);
     hgets(stat.buf, "OBS_MODE", 256, mode);
@@ -180,11 +179,6 @@ int main(int argc, char *argv[]) {
     sprintf(logdir, "%s/BEAM%d_%d", datadir, gpu, (int)getpid());
     sprintf(filename, "%s/%s-%s.log", logdir, basename, hostname);
     sprintf(logname,"%s_%d.log", LOG_FILENAME, (int)getpid());
-
-    char *source_split = strrchr(source, '/');
-    if (source_split != NULL) {
-        snprintf(source_dir, source_split - source + 1, "%s", source);
-    }
 
     sprintf(script,"%s/SCRIPTS/%s_script.sh", datadir, basename);
     sprintf(script_fast,"%s/SCRIPTS/%s_script_fast.sh", datadir, basename);
@@ -197,9 +191,6 @@ int main(int argc, char *argv[]) {
         //######## mkdir DATA/SOURCE
         //sprintf(string,"mkdir %s/DATA/%s 2>&1\n",datadir,source); fputs(string,pfo_slow); fputs(string,pfo_fast);
         //sprintf(string,"if [ \"$1\" !=  \"\" ] && [ \"$1\" !=  \"rsync\" ]; then echo 'error: '$1' is not a valide option (rsync or empty is valide)' ; exit; fi\n"); fputs(string,pfo_fast);
-        if (source_split != NULL) {
-            sprintf(string,"ssh nfrplsobs@databfnfrdt 'mkdir /data/nenufar-pulsar/DATA/%s' 2>&1\n",source_dir); fputs(string,pfo_slow); fputs(string,pfo_fast);
-        }
         sprintf(string,"ssh nfrplsobs@databfnfrdt 'mkdir /data/nenufar-pulsar/DATA/%s' 2>&1\n",source); fputs(string,pfo_slow); fputs(string,pfo_fast);
         sprintf(strlog, "script_mkdir directory %s/DATA/%s on data", datadir,source);
         log_info("nuppi_daq_dedisp", strlog);
@@ -269,9 +260,9 @@ int main(int argc, char *argv[]) {
         sprintf(string, "ssh nfrplsobs@databfnfrdt 'ln /data/nenufar-pulsar/%.4s/%.4s/%.2s/%s* /data/nenufar-pulsar/DATA/%s/%s/' 2>&1\n", projid, year, month, basename, source, mode); fputs(string,pfo_slow); fputs(string,pfo_fast);
         //######## cleaning log files and dir
         sprintf(string, "rm -r %s 2>&1\n", logdir); fputs(string,pfo_slow); fputs(string,pfo_fast);
-        sprintf(string, "mv `ls %s/PARSET-at-*.parset | grep -v %s` %s/OLD_parset/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
-        sprintf(string, "mv `ls %s/PSETUP-at-*.log | grep -v %s` %s/OLD_LOG/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
-        sprintf(string, "mv `ls %s/SHELL--at-*.log | grep -v %s` %s/OLD_LOG/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
+        //sprintf(string, "mv `ls %s/PARSET-at-*.parset | grep -v %s` %s/OLD_parset/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
+        //sprintf(string, "mv `ls %s/PSETUP-at-*.log | grep -v %s` %s/OLD_LOG/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
+        //sprintf(string, "mv `ls %s/SHELL--at-*.log | grep -v %s` %s/OLD_LOG/ 2>&1\n", datadir, obs_time, datadir); fputs(string,pfo_slow); fputs(string,pfo_fast);
 
     }}
     fclose(pfo_slow);
